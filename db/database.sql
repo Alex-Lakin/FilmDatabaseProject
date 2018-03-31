470324a5dde50c96e1dd1fdcdf7f221c0477dc74
Alex-Lakin <alex.lakin@hotmail.co.uk>
2018-03-31 16:28:07 +0100

    database created

---
+++ db/database.sql
@@ -0,0 +1,22 @@
+
+DROP TABLE films_directors;
+DROP TABLE films;
+DROP TABLE directors;
+
+CREATE TABLE films(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(225),
+  year INT8,
+  rating INT2
+);
+
+CREATE TABLE directors(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(225)
+);
+
+CREATE TABLE films_directors(
+  id SERIAL8 PRIMARY KEY,
+  film_id INT8 REFERENCES films(id),
+  director_id INT8 REFERENCES directors(id)
+);
