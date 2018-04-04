d74b00611901c872778d37c03c825f0490c1be61
Alex-Lakin <alex.lakin@hotmail.co.uk>
2018-04-04 16:29:32 +0100

    genre page does not load

--- models/genre.rb
+++ models/genre.rb
@@ -16,7 +16,7 @@ class Genre
     sql = "INSERT INTO genres (genre)
           VALUES ($1)
           RETURNING id"
-    values = [@name]
+    values = [@genre]
     result = SqlRunner.run(sql, values)
     @id = result.first()["id"].to_i
   end
